795c316693b07b98def75c2e23f4ecf48eecb0e7
KarkiAdi <adityakarki728@gmail.com>
2024-04-16 23:56:43 -0500

    Added post feature for post form

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -21,7 +21,7 @@ interface CreatePostFormState {
     }
 };
 
-export async function createPost(formState: CreatePostFormState, formData: FormData): Promise<CreatePostFormState> {
+export async function createPost(slug : string, formState: CreatePostFormState, formData: FormData): Promise<CreatePostFormState> {
     const result = createPostSchema.safeParse({
         title: formData.get('title'),
         content: formData.get('content')
@@ -34,8 +34,56 @@ export async function createPost(formState: CreatePostFormState, formData: FormD
         }
     };
 
-    return {
-        errors: {}
+    // Notify user to login before creating a post
+    const session = await auth();
+    if (!session || !session.user) {
+        return {
+            errors: {
+                _form: ['You must be signed in to do this.']
+            }
+        };
     }
-    //TODO: revalidate the topic show page
+
+    // Handle topic not found in database
+    const topic = await postgresDb.topic.findFirst({
+        where: { slug }
+    });
+    if (!topic) {
+        return {
+            errors: {
+                _form: ['Cannon find topic']
+            }
+        }
+    };
+    
+    // Handle database errors
+    let post: Post;
+    try {
+        post = await postgresDb.post.create({
+            data: {
+                title: result.data.title,
+                content: result.data.content,
+                userId: session.user.id,
+                topicId: topic.id
+            }
+        })
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            return {
+                errors: {
+                    _form: [err.message]
+                }
+            }
+        } else {
+            return {
+                errors: {
+                    _form: ['Something went wrong. Failed to create post']
+                }
+            }
+        }
+    }
+    
+    // Handle caching and path redirection
+    revalidatePath(paths.topicShow(slug));
+    redirect(paths.postShow(slug, post.id));
 }
--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -11,8 +11,11 @@ import {
 } from '@nextui-org/react';
 import * as actions from '@/actions';
 import FormButton from '@/components/common/form-button';
-export default function PostCreateForm(){
-    const [formState, action] = useFormState(actions.createPost, {
+interface PostCreateFormProps {
+    slug: string
+};
+export default function PostCreateForm({ slug }: PostCreateFormProps){
+    const [formState, action] = useFormState(actions.createPost.bind(null, slug), {
         errors: {}
     });
     return (
@@ -28,6 +31,7 @@ export default function PostCreateForm(){
                         <h3 className='text-lg'>Create a Post</h3>
                         <Input isInvalid={!!formState.errors.title} errorMessage={formState.errors.title?.join(', ')} name='title' label='Title' labelPlacement='outside' placeholder='Title' />
                         <Textarea isInvalid={!!formState.errors.content} errorMessage={formState.errors.content?.join(', ')} name='content' label='Content' labelPlacement='outside' placeholder='Content' />
+                        {formState.errors._form ? <div className='text-tiny text-danger rounded p-2 bg-red border border-red-300'>{formState.errors._form?.join(', ')}</div>:  null}
                         <FormButton>
                             Create Post
                         </FormButton>
